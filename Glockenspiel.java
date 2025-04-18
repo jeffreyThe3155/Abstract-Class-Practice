4ac488a115822215dcaefd777a13d1558458f64e
jeffreyThe3155 <jjl28282@gmail.com>
2025-04-18 21:14:25 +0000

    commit

--- Glockenspiel.java
+++ Glockenspiel.java
@@ -1,8 +1,8 @@
 public class Glockenspiel extends Instrument {
     public void play() {
-        System.out.println(this + "is a glock.");
+        System.out.println(this + " is a glock.");
     }
     public void tune() {
-        System.out.println(this + "can be played");
+        System.out.println(this + " can be played.");
     }
 }
--- Violin.java
+++ Violin.java
@@ -1,8 +1,8 @@
 public class Violin extends Instrument {
     public void play() {
-        System.out.println(this + "is a viola.");
+        System.out.println(this + " is a viola.");
     }
     public void tune() {
-        System.out.println(this + "can be played");
+        System.out.println(this + " can be played.");
     }
 }
